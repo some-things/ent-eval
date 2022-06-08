850ec3ef7f2d67c20826669105fe0c731ba7309e
Dustin Nemes <39643802+some-things@users.noreply.github.com>
2022-06-08 00:25:19 -0500

    wip: add car + group ents + user->car edge

--- start/start.go
+++ start/start.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/some-things/ent-eval/ent"
+	"github.com/some-things/ent-eval/ent/car"
 	"github.com/some-things/ent-eval/ent/user"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -26,6 +28,7 @@ func main() {
 	}
 }
 
+// Users
 func CreateUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
 	u, err := client.User.Create().SetAge(30).SetName("a8m").Save(ctx)
 	if err != nil {
@@ -49,4 +52,50 @@ func QueryUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
 
 	log.Println("user returned: ", u)
 	return u, nil
+}
+
+// Cars
+func CreateCars(ctx context.Context, client *ent.Client) (*ent.User, error) {
+	// Create a new car with model "Tesla"
+	tesla, err := client.Car.
+		Create().
+		SetModel("Tesla").
+		SetRegisteredAt(time.Now()).
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating car: %w", err)
+	}
+	log.Println("car was created: ", tesla)
+
+	// Create a new car with the model "Ford"
+	ford, err := client.Car.Create().SetModel("Ford").SetRegisteredAt(time.Now()).Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating car: %w", err)
+	}
+	log.Println("car was created: ", ford)
+
+	// Create a new user and add it to the 2 cars
+	a8m, err := client.User.Create().SetAge(30).SetName("a8m").AddCars(tesla, ford).Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating user: %w", err)
+	}
+	log.Println("user was created: ", a8m)
+
+	return a8m, nil
+}
+
+func QueryCars(ctx context.Context, a8m *ent.User) error {
+	cars, err := a8m.QueryCars().All(ctx)
+	if err != nil {
+		return fmt.Errorf("failed querying cars: %w", err)
+	}
+	log.Println("cars returned: ", cars)
+
+	// Filtering specific cars
+	ford, err := a8m.QueryCars().Where(car.Model("Ford")).Only(ctx)
+	if err != nil {
+		return fmt.Errorf("failed querying cars: %w", err)
+	}
+	log.Println(ford)
+	return nil
 }
